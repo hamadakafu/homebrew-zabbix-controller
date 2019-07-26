5cbd59c418b1feb45ad45601508b1ba8164a6197
hamadakafu <kafu@3-shake.com>
2019-07-26 13:48:05 +0900

    add homebrew

---
+++ homebrew-zabbix-controller.rb
@@ -0,0 +1,32 @@
+class HomebrewZabbixController < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/hamadakafu/homebrew-zabbix-controller/releases/download/v0.1.11/zabbixctl"
+  sha256 "9269cd74e89c3c569a9c056cd1f83612f3f0e9974da3d2aa60fd666703c88ce9"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-zabbix-controller`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
